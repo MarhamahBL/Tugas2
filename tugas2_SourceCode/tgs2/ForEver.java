ffae83aeff7530c49b19d8e946562d5d476bbb16
MarhamahBL <127584572+MarhamahBL@users.noreply.github.com>
2023-03-27 01:11:14 +0800

    Add files via upload

---
+++ tugas2_SourceCode/tgs2/ForEver.java
@@ -0,0 +1,22 @@
+/* NIM      : 13020210247
+Nama        : Marhamah BL
+Hari/Tgl    : Minggu, 26 Maret 2023
+Waktu       : 10.42  PM */
+package tgs2;
+
+public class ForEver {
+
+    /**
+    * @param args
+    */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        /* Program */
+        System.out.println("Program akan looping, akhiri dengan ^c");
+        while (true){
+            System.out.print ("Print satu baris ....\n");
+        }
+    
+    }
+    
+}
