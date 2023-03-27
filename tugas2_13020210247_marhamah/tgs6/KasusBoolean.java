34dad1908243e467a701619a5f8d1c4c13b0fa2a
MarhamahBL <127584572+MarhamahBL@users.noreply.github.com>
2023-03-27 23:50:48 +0800

    Add files via upload

---
+++ tugas2_13020210247_marhamah/tgs6/KasusBoolean.java
@@ -0,0 +1,29 @@
+/* NIM      : 13020210247
+Nama        : Marhamah BL
+Hari/Tgl    : Minggu, 26 Maret 2023
+Waktu       : 01.01  PM */
+package tgs6;
+
+/* Eksrpesi kondisional dengan boolean */
+public class KasusBoolean {
+
+    /**
+    * @param args
+    */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        /* Kamus */
+        boolean bool;
+        /* Algoritma */
+        bool = true;
+        if(bool) {
+            System.out.print("true\n");
+        } else
+            System.out.print("false\n");
+        if(!bool) {
+            System.out.print("salah\n");
+        } else
+            System.out.print("benar\n");
+
+    } 
+}
